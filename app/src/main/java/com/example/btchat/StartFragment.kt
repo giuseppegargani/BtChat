7c0b38d1839a65c2d099bf51438eb3935c9830bf
giuseppegargani <sentierodisole1@yahoo.it>
2021-10-27 15:20:29 +0200

    aggiunto fragment

---
+++ app/src/main/java/com/example/btchat/StartFragment.kt
@@ -0,0 +1,20 @@
+package com.example.btchat
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import com.example.btchat.databinding.FragmentStartBinding
+
+class StartFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val binding = DataBindingUtil.inflate<FragmentStartBinding>(inflater, R.layout.fragment_start, container, false)
+        return binding.root
+    }
+}
